9eff8b3dd0d35f21f54ecc7e604daa92373a4e40
Kuzynowski, Tomasz <Tomasz.Kuzynowski@axxiomehealth.com>
2021-06-02 11:27:10 +0200

    express endpoint with CAP API

--- my-bookshop/srv/cat-service.cds
+++ my-bookshop/srv/cat-service.cds
@@ -1,11 +1,10 @@
 using { Country, managed } from '@sap/cds/common';
 using my.bookshop as bookshop from '../db/data-model';
 
-service CatalogService {
+service CatalogService2 {
 
   entity Books @readonly as projection on bookshop.Books;
   entity Authors @readonly as projection on  bookshop.Authors;
   entity Orders @insertonly as projection on  bookshop.Orders;
-  entity Logs @readonly as projection on  bookshop.Logs;
 
 }
--- my-bookshop/srv/cat-service.js
+++ my-bookshop/srv/cat-service.js
@@ -1,14 +1,40 @@
-const { Log, LogLevel } = require('../gen/srv/srv/Log');
 const cds = require('@sap/cds')
+var bodyParser = require('body-parser')
 module.exports = (srv) => {
 
   const { Books } = cds.entities('my.bookshop')
   const { Logs } = cds.entities('my.bookshop')
 
+  // create application/json parser
+  // create application/x-www-form-urlencoded parser
+  var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+  var app = cds.app;
+  app.post("/logs", urlencodedParser, (req, res) => {
+    let aLog = req.body;
+    console.log(req.body);
+    insertLog(req, Logs, aLog);
+    console.log("inserted into Logs: ", aLog)
+    res.status(200).end();
+  });
+
+  app.get("/logs", (req, res, next) => {
+    let query = SELECT.from(Logs)
+    let logPromise = cds.run(query)
+    logPromise.then(value => {
+      console.log('Promise resolved');
+      console.log(JSON.stringify(value));
+      res.end(JSON.stringify(value));
+    }, reject => {
+      console.log('rejected')
+      console.log(reject)
+    });
+  });
+
   srv.on('READ', 'Books', (req) => {
-    let aLog = new Log("anExampleTenant", LogLevel.DEBUG);
-    const affectedRows = insertLog(req, Logs, aLog);
-    console.log("insterted into Logs that somebody tried to get books. New rows added {}", affectedRows.count)
+    let aLog = { "tenantId": "anExampleTenant", "level": "D" }
+    insertLog(req, Logs, aLog);
+    console.log("inserted into Logs that somebody tried to get books.")
     let query = SELECT.from(Books)
     return cds.run(query);
   })
@@ -16,8 +42,7 @@ module.exports = (srv) => {
 
 function insertLog(req, Logs, aLog) {
   const tx = cds.transaction(req);
-  const affectedRows = tx.run(
+  tx.run(
     INSERT.into(Logs).entries(aLog)
   );
-  return affectedRows;
 }
